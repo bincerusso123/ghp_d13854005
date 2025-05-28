45b4cf947244bfc83fd9856258b2a6ef42cb6eda
bincerusso123 <D13854005@ntu.edu.tw>
2025-05-28 15:01:36 +0800

    Update Assignment No.3 - HRM.R

--- Assignment No.3 - HRM.R
+++ Assignment No.3 - HRM.R
@@ -1,4 +1,5 @@
 # Health Research Methods - Assignment No. 3
+# June 3, 2025
 
 setwd("/Users/bincerussocrieta/Desktop/HRM - Assignment No.3/")
 dengue<-read.csv(file = 'dengue_assignment.csv', header = TRUE)
@@ -10,4 +11,5 @@ total_cases_2014 <- dengue[dengue$year == 2014, "case_number"] |> sum()
 
 # Result
 total_cases_2014
+
 
