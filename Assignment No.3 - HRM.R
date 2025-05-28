a6338fe588af085b20364ed5fa2a989df55d339a
bincerusso123 <D13854005@ntu.edu.tw>
2025-05-28 14:45:09 +0800

    Dengue Code for 2014 Cases

---
+++ Assignment No.3 - HRM.R
@@ -0,0 +1,13 @@
+# Health Research Methods - Assignment No. 3
+
+setwd("/Users/bincerussocrieta/Desktop/HRM - Assignment No.3/")
+dengue<-read.csv(file = 'dengue_assignment.csv', header = TRUE)
+
+head (dengue)
+
+# Filter data for 2014 and sum the cases
+total_cases_2014 <- dengue[dengue$year == 2014, "case_number"] |> sum()
+
+# Result
+total_cases_2014
+
